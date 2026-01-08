13f9d78bca38af2f67d5d647eef8bf47e01489c5
ming <liumingmad.gmail.com>
2026-01-08 08:59:22 +0800

    demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,17 +12,14 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button class="count-btn" type="button" @click="count++">count is {{ count }}</button>
   </div>
 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  .count-btn {
+    color: brown;
+    background-color: white;
+  }
 </style>
