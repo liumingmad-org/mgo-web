48b043cac55c44d8f18dbe69bcb7682220049739
ming <liumingmad.gmail.com>
2026-01-11 10:42:21 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import { addTowNum, haha } from '../utils/test'
 
 defineProps({
   msg: String,
@@ -7,20 +8,39 @@ defineProps({
 
 const count = ref(0)
 
+
+let name = haha
+
+let nani = reactive({
+  x: 10,
+  y: 20,
+})
+
 function add() {
   count.value++;
+
+  console.log(addTowNum(100, count.value))
+
+  nani.x++
+  nani.y++
 }
 
-let name = "Alice"
+let show = ref(true)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
   <h1>{{ name }}</h1>
 
+  <input v-model="nani.x" />
+  <p>你输入的是{{ nani.x }}, {{ nani.y }}</p>
+
   <div class="card">
     <button class="count-btn" type="button" @click="add()">count is {{ count }}</button>
   </div>
+
+  <button class="count-btn" type="button" @click="show=!show">show</button>
+  <p v-if="show" class="show-view">look me</p>
 
 </template>
 
@@ -28,5 +48,9 @@ let name = "Alice"
   .count-btn {
     color: brown;
     background-color: white;
+  }
+
+  .show-view {
+    color: greenyellow;
   }
 </style>
---
+++ src/utils/test.ts
@@ -0,0 +1,5 @@
+export function addTowNum(a: number, b: number) {
+  return a + b
+}
+
+export let haha: string = "from ts"
